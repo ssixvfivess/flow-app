7613ac6220c2b5af84677f28ac1e964f954a2c42
ssixvfivess <ssixvfivess@yandex.ru>
2025-04-04 10:10:44 +0500

    #18 Fix (a little bit) and commented

--- app/src/main/java/com/psutools/reminder/ui/fragments/RoutesFragment.kt
+++ app/src/main/java/com/psutools/reminder/ui/fragments/RoutesFragment.kt
@@ -9,10 +9,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.psutools.reminder.base.arch.BaseFragment
 import com.psutools.reminder.base.arch.ScreenState
 import com.psutools.reminder.databinding.FragmentRoutesBinding
-import com.psutools.reminder.ui.presentation.list.trip.TripDataListState
 import com.psutools.reminder.ui.presentation.routes.RoutesDataListAdapter
 import com.psutools.reminder.ui.presentation.routes.RoutesDataListState
-import com.psutools.reminder.ui.presentation.routes.RoutesDataListViewModel
+import com.psutools.reminder.ui.presentation.routes.RoutesViewModel
 import com.psutools.reminder.utils.ui.SnackbarManager
 import com.psutools.reminder.utils.ui.collectWithLifecycle
 import com.psutools.reminder.utils.ui.tools.switcher.ContentStateSwitcher
@@ -28,42 +27,23 @@ class RoutesFragment : BaseFragment<FragmentRoutesBinding>() {
         return FragmentRoutesBinding.inflate(layoutInflater)
     }
 
-    override fun onResume() {
-        super.onResume()
-        setCalendarToCurrentDate()
+    private val viewModel: RoutesViewModel by viewModels<RoutesViewModel>()
+    private val onClickListener = { text: String ->
+        SnackbarManager.createSnackbar(viewBinding.root, "Item: $text")
     }
-
-    private fun setCalendarToCurrentDate() {
-        val today = Calendar.getInstance()
-        viewBinding.calendarView.date = today.timeInMillis
-    }
-
-    private val viewModel: RoutesDataListViewModel by viewModels<RoutesDataListViewModel>()
     private lateinit var routesDataListAdapter: RoutesDataListAdapter
-
     private lateinit var contentStateSwitcher: ContentStateSwitcher<ContentState>
 
     override fun initUi() {
         val isFirstLaunch = viewModel.hasContent
 
         setupRecycler()
+        setCalendarToCurrentDate()
         setupStateSwitcher(isFirstLaunch)
         setupCalendar()
 
         if (!isFirstLaunch) {
             viewModel.loadData()
-        }
-    }
-
-    private fun setupCalendar() {
-        viewBinding.calendarView.setOnDateChangeListener { _: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
-            val selectedDate = DateTime()
-                .withYear(year)
-                .withMonthOfYear(month + 1)
-                .withDayOfMonth(dayOfMonth)
-                .withTimeAtStartOfDay()
-
-            viewModel.setSelectedDate(selectedDate)
         }
     }
 
@@ -86,17 +66,38 @@ class RoutesFragment : BaseFragment<FragmentRoutesBinding>() {
         viewBinding.contentRecycler.adapter = routesDataListAdapter
     }
 
+    private fun setCalendarToCurrentDate() {
+        val today = Calendar.getInstance()
+        viewBinding.calendarView.date = today.timeInMillis
+    }
+
     private fun setupStateSwitcher(isFirstLaunch: Boolean) {
         contentStateSwitcher = createContentStateSwitcher(
             initialState = if (isFirstLaunch) ContentState.LOADING else ContentState.CONTENT,
             mapper = { state ->
                 when (state) {
-                    ContentState.CONTENT -> listOf(viewBinding.contentRecycler)
+                    ContentState.CONTENT -> listOf(
+                        viewBinding.contentRecycler,
+                        viewBinding.calendarView, viewBinding.addFab
+                    )
+
                     ContentState.LOADING -> listOf(viewBinding.progress)
                     else -> emptyList()
                 }
             }
         )
+    }
+
+    private fun setupCalendar() {
+        viewBinding.calendarView.setOnDateChangeListener { _: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
+            val selectedDate = DateTime()
+                .withYear(year)
+                .withMonthOfYear(month + 1)
+                .withDayOfMonth(dayOfMonth)
+                .withTimeAtStartOfDay()
+
+            viewModel.setSelectedDate(selectedDate)
+        }
     }
 
     private fun showContent(stateData: RoutesDataListState) {
@@ -108,9 +109,5 @@ class RoutesFragment : BaseFragment<FragmentRoutesBinding>() {
 
     private fun showLoading() {
         contentStateSwitcher.switchState(ContentState.LOADING)
-    }
-
-    private val onClickListener = { text: String ->
-        SnackbarManager.createSnackbar(viewBinding.root, "Item: $text")
     }
 }
--- app/src/main/java/com/psutools/reminder/ui/presentation/routes/RoutesViewModel.kt
+++ app/src/main/java/com/psutools/reminder/ui/presentation/routes/RoutesViewModel.kt
@@ -6,8 +6,6 @@ import com.psutools.reminder.base.arch.ScreenState
 import com.psutools.reminder.base.coroutines.CoroutineDispatchers
 import com.psutools.reminder.domain.model.trip.TripData
 import com.psutools.reminder.domain.usecase.trip.GetTripDataListUseCase
-import com.psutools.reminder.ui.presentation.list.trip.TripDataListState
-import com.psutools.reminder.ui.presentation.list.trip.TripDataListUiMapper
 import com.psutools.reminder.utils.coroutines.tryLaunch
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -18,14 +16,14 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class RoutesDataListViewModel @Inject constructor(
+class RoutesViewModel @Inject constructor(
     private val getRoutesDataListUseCase: GetTripDataListUseCase,
     private val coroutineDispatchers: CoroutineDispatchers,
     private val routesDataListUiMapper: RoutesDataListUiMapper,
 ) : ViewModel() {
 
-    private val _state: MutableStateFlow<ScreenState<RoutesDataListState>> = MutableStateFlow(
-        ScreenState.Loading)
+    private val _state: MutableStateFlow<ScreenState<RoutesDataListState>> =
+        MutableStateFlow(ScreenState.Loading)
     val state: Flow<ScreenState<RoutesDataListState>> = _state.asStateFlow()
 
     val hasContent: Boolean
@@ -35,15 +33,12 @@ class RoutesDataListViewModel @Inject constructor(
     private var selectedDate: DateTime? = null
 
     fun loadData() {
-        viewModelScope.tryLaunch(
-            doOnLaunch = {
-                allTrips = getRoutesDataListUseCase()
-                filterAndMapTrips()
-            },
-            doOnError = { error ->
-                Timber.tag(TAG).e(error, "An error occurred while loading data")
-            },
-            coroutineContext = coroutineDispatchers.io()
+        viewModelScope.tryLaunch(doOnLaunch = {
+            allTrips = getRoutesDataListUseCase()
+            filterAndMapTrips()
+        }, doOnError = { error ->
+            Timber.tag(TAG).e(error, "An error occurred while loading data")
+        }, coroutineContext = coroutineDispatchers.io()
         )
     }
 
@@ -66,6 +61,6 @@ class RoutesDataListViewModel @Inject constructor(
     }
 
     private companion object {
-        const val TAG = "RoutesDataListViewModel"
+        const val TAG = "RoutesViewModel"
     }
 }

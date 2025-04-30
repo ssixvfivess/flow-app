0046a1bc81162c3eb43c50c463659a709fb5e4b3
ssixvfivess <ssixvfivess@yandex.ru>
2025-04-30 17:55:23 +0500

    #27 Fix RoutesFragment

--- app/src/main/java/com/psutools/reminder/ui/fragments/RoutesFragment.kt
+++ app/src/main/java/com/psutools/reminder/ui/fragments/RoutesFragment.kt
@@ -1,9 +1,11 @@
 package com.psutools.reminder.ui.fragments
 
+import android.app.Activity
 import android.icu.util.Calendar
 import android.os.Handler
 import android.os.Looper
 import android.widget.CalendarView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.psutools.reminder.app.navigation.Router
@@ -112,7 +114,18 @@ class RoutesFragment : BaseFragment<FragmentRoutesBinding>() {
         contentStateSwitcher.switchState(ContentState.LOADING)
     }
 
+    private val deleteResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            viewBinding.contentRecycler.postDelayed({
+                viewModel.loadData()
+            }, 300)
+        }
+    }
+
     private val onClickListener = { tripId: String ->
-        startActivity(router.createRouteDetailsIntent(requireContext(), tripId))
+        val intent = router.createRouteDetailsIntent(requireContext(), tripId)
+        deleteResultLauncher.launch(intent)
     }
 }
